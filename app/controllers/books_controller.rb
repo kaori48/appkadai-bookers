b81feae4e4c67ddb829c2f2918b4a0d5dfbd8955
古家歌織 <furuyakaori@kaori-f.local>
2020-01-09 16:57:07 +0900

    [Start2]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,29 +3,58 @@ class BooksController < ApplicationController
 #top画画
   def top
   end
+
 #詳細画面
   def index
-  	#データ新しい順にを並べる機能
-  	@books = Book.all.order(id: "ASC")
+  	@books = Book.all.order(id: "ASC")#データ新しい順にを並べる機能
   	#投稿機能,viewに渡すための空のモデルオブジェクト
   	@book = Book.new
   end
-# 投稿内容確認
-  def show
-  	@book = Book.find(params[:id])
-  end
-
 
 #フォーム投稿
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id) #特定のidの投稿内容表示
+  	@book = Book.new(book_params)
+  	if
+        @book.save
+        flash[:ok] = 'Book was successfully created.'   #作成成功メッセージ出す
+        redirect_to book_path(@book.id) #showへリダイレクト。入力で割り振られたidを表示
+      else
+        @books = Book.all.order(id: "ASC") #失敗時の処理がわからない？
+        render action: :index #詳細画面??????に戻る
+        #バリデーションのエラーはmodelで設定
+    end
   end
+
+# 投稿内容確認
+  def show
+    @book = Book.find(params[:id])
+  end
+
 #投稿
   def edit
   	@book = Book.find(params[:id])
   end
+
+#更新
+  def update#更新できたらのif描きたい
+    book = Book.find(params[:id])
+    if
+      book.update(book_params)
+      flash[:ok] = 'Book was successfully updated.'   #作成成功メッセージ出す
+      redirect_to book_path(book.id)#showへリダイレクト,idsyotoku
+    else
+      #失敗時の処理？
+      render action: :edit#投稿画面に戻る？
+      #バリデーションのエラーはmodelの方で行う
+    end
+  end
+#削除
+ def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+ end
+
 
 private
  def book_params
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+#バリデーション設定、空じゃないか
+validates :title, presence: true
+validates :body, presence: true
 end
